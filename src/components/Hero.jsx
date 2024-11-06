8b239f43f20662f0277728b2b060854ada714c38
mohd-afnan-shahab <mohdafnan545@gmail.com>
2024-11-06 12:33:30 +0530

    Initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/mohd-afnan-shahab/Rapid-Content-Gatherer", "_blank")
           }
           className='black_btn'
         >
